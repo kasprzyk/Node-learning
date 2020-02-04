8326c53f8cd39cd6b5a94b9a615588f7c2222e66
kasprzyk <piotkas1@gmail.com>
2020-02-04 17:23:18 +0100

    import and use of first npm module

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,7 +1,10 @@
 const getNotes = require("./notes.js");
-
+const validator = require("validator");
 const note = getNotes();
 console.log(note);
+
+console.log(validator.isEmail("test@wp.pl"));
+console.log(validator.isURL("testetestst"));
 // const add = require("./utils.js");
 
 // const sum = add(4, 10);
