291788b4bf3f77f6f70c5624ffbd97e1ec875884
kasprzyk <piotkas1@gmail.com>
2020-02-18 17:00:18 +0100

    Printing in color using chalk

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,10 +1,11 @@
 const getNotes = require("./notes.js");
 const validator = require("validator");
 const note = getNotes();
-console.log(note);
+const chalk = require("chalk");
+console.log(chalk.underline.blue(note));
 
-console.log(validator.isEmail("test@wp.pl"));
-console.log(validator.isURL("testetestst"));
+// console.log(validator.isEmail("test@wp.pl"));
+// console.log(validator.isURL("testetestst"));
 // const add = require("./utils.js");
 
 // const sum = add(4, 10);
