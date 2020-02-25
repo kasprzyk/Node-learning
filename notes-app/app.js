e472b54684835f542837a62660d4deed985de5a2
kasprzyk <piotkas1@gmail.com>
2020-02-25 16:26:55 +0100

    Getting input from users

--- notes-app/app.js
+++ notes-app/app.js
@@ -2,11 +2,12 @@ const getNotes = require("./notes.js");
 const validator = require("validator");
 const note = getNotes();
 const chalk = require("chalk");
-console.log(chalk.underline.blue(note));
 
-// console.log(validator.isEmail("test@wp.pl"));
-// console.log(validator.isURL("testetestst"));
-// const add = require("./utils.js");
-
-// const sum = add(4, 10);
-// console.log(sum);
+const command = process.argv[2];
+if (command === "add") {
+  console.log("Adding note");
+} else if (command === "remove") {
+  console.log("removing note");
+} else {
+  console.log("wrong command");
+}
