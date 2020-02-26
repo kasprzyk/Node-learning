fa2e900efde319d73511bcddfcb63e033b0b493e
kasprzyk <piotkas1@gmail.com>
2020-02-26 17:01:54 +0100

    readFileSync JSON

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,12 +1,16 @@
 const fs = require("fs");
-const book = {
-  title: "test",
-  author: "test"
-};
+// const book = {
+//   title: "test",
+//   author: "test"
+// };
 
-const bookJSON = JSON.stringify(book);
-fs.writeFileSync("1-json.json", bookJSON);
-console.log(bookJSON);
+// const bookJSON = JSON.stringify(book);
+// fs.writeFileSync("1-json.json", bookJSON);
+// console.log(bookJSON);
 
-const parsedData = JSON.parse(bookJSON);
-console.log(parsedData);
+// const parsedData = JSON.parse(bookJSON);
+// console.log(parsedData);
+const dataBuffer = fs.readFileSync("1-json.json");
+const dataJSON = dataBuffer.toString();
+const data = JSON.parse(dataJSON);
+console.log(data.title);
