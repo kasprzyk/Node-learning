66fc5f6c8b9f64ca14e53c4a7a2ef3cefaec24f1
kasprzyk <piotkas1@gmail.com>
2020-03-02 17:36:51 +0100

    Adding a note #2

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -6,11 +6,17 @@ const getNotes = function() {
 
 const addNote = function(title, body) {
   const notes = loadNotes();
-  notes.push({
-    title: title,
-    body: body
-  });
-  saveNotes(notes);
+  const duplicateNotes = notes.filter(note => note.title === title);
+  if (duplicateNotes.length === 0) {
+    notes.push({
+      title: title,
+      body: body
+    });
+    saveNotes(notes);
+    console.log("New note added");
+  } else {
+    console.log("Note title taken");
+  }
 };
 
 const loadNotes = function() {
