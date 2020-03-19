a81fae8fbf3e717b6d6c8648810757b5c987abf2
kasprzyk <piotkas1@gmail.com>
2020-03-19 15:11:25 +0100

    handling errors #1

--- weather-app/app.js
+++ weather-app/app.js
@@ -4,16 +4,31 @@ const url =
   "https://api.darksky.net/forecast/c24c203d764b06415ad1b07d351a75e2/37.8267,-122.4233?units=si";
 
 request({ url: url, json: true }, (error, response) => {
-  let currently = response.body.currently;
-  console.log(
-    `It is currently ${currently.temperature} degree out and ${currently.precipProbability}% chance of rain`
-  );
+  if (error) {
+    console.log(`error: ${error.body.code}`);
+  } else if (response.body.error) {
+    console.log("Unable to find location");
+  } else {
+    let currently =
+      response && response.body && response.body.currently
+        ? response.body.currently
+        : null;
+    if (currently) {
+      console.log(
+        `It is currently ${currently.temperature} degree out and ${currently.precipProbability}% chance of rain`
+      );
+    }
+  }
 });
 const geocodeUrl =
   "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoicGlvdGthcyIsImEiOiJjazd4aHJ1ZnEwMmgwM2dtbnJ2aGw0MWpzIn0.K1Ha3OHoH2rugffhUFpLig";
 
 request({ url: geocodeUrl, json: true }, (error, response) => {
-  let lat = response.body.features[0].center[1];
-  let lng = response.body.features[0].center[0];
-  console.log(`lat: ${lat}, lng: ${lng}`);
+  if (error) {
+    console.log(`error: ${error.body.code}`);
+  } else {
+    let lat = response.body.features[0].center[1];
+    let lng = response.body.features[0].center[0];
+    console.log(`lat: ${lat}, lng: ${lng}`);
+  }
 });
