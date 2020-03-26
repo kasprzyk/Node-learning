2941d0501a5b2e07c09e6259ada89e4eec249362
kasprzyk <piotkas1@gmail.com>
2020-03-26 16:21:41 +0100

    destructuring #2

--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -7,19 +7,19 @@ const forecast = (latitude, longitude, callback) => {
     "," +
     longitude;
 
-  request({ url: url, json: true }, (error, response) => {
+  request({ url: url, json: true }, (error, { body }) => {
     if (error) {
       callback("Unable to connect to weather service!", undefined);
-    } else if (response.body.error) {
+    } else if (body.error) {
       callback("Unable to find location", undefined);
     } else {
       callback(
         undefined,
-        response.body.daily.data[0].summary +
+        body.daily.data[0].summary +
           " It is currently " +
-          response.body.currently.temperature +
+          body.currently.temperature +
           " degress out. There is a " +
-          response.body.currently.precipProbability +
+          body.currently.precipProbability +
           "% chance of rain."
       );
     }
--- weather-app/utils/geocode.js
+++ weather-app/utils/geocode.js
@@ -6,16 +6,16 @@ const geocode = (address, callback) => {
     encodeURIComponent(address) +
     ".json?access_token=pk.eyJ1IjoicGlvdGthcyIsImEiOiJjazd4aHJ1ZnEwMmgwM2dtbnJ2aGw0MWpzIn0.K1Ha3OHoH2rugffhUFpLig";
 
-  request({ url: url, json: true }, (error, response) => {
+  request({ url: url, json: true }, (error, { body }) => {
     if (error) {
       callback("Unable to connect to location services!", undefined);
-    } else if (response.body.features.length === 0) {
+    } else if (body.features.length === 0) {
       callback("Unable to find location. Try another search.", undefined);
     } else {
       callback(undefined, {
-        latitude: response.body.features[0].center[0],
-        longitude: response.body.features[0].center[1],
-        location: response.body.features[0].place_name
+        latitude: body.features[0].center[0],
+        longitude: body.features[0].center[1],
+        location: body.features[0].place_name
       });
     }
   });
