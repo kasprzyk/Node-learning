7f42f45c5b9f912454c4db5349b55a92223903a7
kasprzyk <piotkas1@gmail.com>
2020-03-29 23:17:06 +0200

    serving up html and json

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,18 +3,30 @@ const express = require("express");
 const app = express();
 
 app.get("", (req, res) => {
-  res.send("Express #1");
+  res.send("<h1>Weather</h1>");
 });
 
 app.get("/help", (req, res) => {
-  res.send("Express #2");
+  res.send([
+    {
+      name: "Piotr",
+      age: 27
+    },
+    {
+      name: "Piotr",
+      age: 27
+    }
+  ]);
 });
 
-app.get("/title", (req, res) => {
-  res.send("title page");
+app.get("/about", (req, res) => {
+  res.send("<h1>About</h1>");
 });
 app.get("/weather", (req, res) => {
-  res.send("weather page");
+  res.send({
+    forecast: "test",
+    location: "Poland"
+  });
 });
 app.listen(3000, () => {
   console.log("Server is up on port 3000");
