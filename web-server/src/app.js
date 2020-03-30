ad14b46282c79aa2bff6a31b37d9e15d02bd507d
kasprzyk <piotkas1@gmail.com>
2020-03-30 17:12:08 +0200

    serving up static assets

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,27 +1,14 @@
+const path = require("path");
 const express = require("express");
 
 const app = express();
+const publicDirectoryPath = path.join(__dirname, "../public");
+app.use(express.static(publicDirectoryPath));
 
 app.get("", (req, res) => {
   res.send("<h1>Weather</h1>");
 });
 
-app.get("/help", (req, res) => {
-  res.send([
-    {
-      name: "Piotr",
-      age: 27
-    },
-    {
-      name: "Piotr",
-      age: 27
-    }
-  ]);
-});
-
-app.get("/about", (req, res) => {
-  res.send("<h1>About</h1>");
-});
 app.get("/weather", (req, res) => {
   res.send({
     forecast: "test",
