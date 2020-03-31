480b20b09d5b28218e9ac2ca208aeb9a0b9bcf78
kasprzyk <piotkas1@gmail.com>
2020-03-31 23:23:49 +0200

    dynamic pages with templating

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,11 +3,32 @@ const express = require("express");
 
 const app = express();
 const publicDirectoryPath = path.join(__dirname, "../public");
+app.set("view engine", "hbs");
 app.use(express.static(publicDirectoryPath));
 
 app.get("", (req, res) => {
-  res.send("<h1>Weather</h1>");
+  res.render("index", {
+    title: "Weather App",
+    name: "Piotr Kasprzycki"
+  });
 });
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About",
+    name: "Piotr Kasprzycki"
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    message: "help message"
+  });
+});
+
+// app.get("", (req, res) => {
+//   res.send("<h1>Weather</h1>");
+// });
 
 app.get("/weather", (req, res) => {
   res.send({
