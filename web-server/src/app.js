e8ae577f6fed9362e40162a51b65557a165b4853
kasprzyk <piotkas1@gmail.com>
2020-04-02 16:50:30 +0200

    Templating

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,14 +1,17 @@
 const path = require("path");
 const express = require("express");
 const app = express();
+const hbs = require("hbs");
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, "../public");
-const viewsPath = path.join(__dirname, "../templates");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
 
 // Setup handlebars engine and views location
 app.set("view engine", "hbs");
 app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
 
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
@@ -29,7 +32,9 @@ app.get("/about", (req, res) => {
 
 app.get("/help", (req, res) => {
   res.render("help", {
-    message: "help message"
+    message: "help message",
+    title: "Help",
+    name: "Piotr Kasprzycki"
   });
 });
 
