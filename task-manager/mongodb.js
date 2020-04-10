93cba0369b725f39a5e1c0e738cc308e274e49e5
kasprzyk <piotkas1@gmail.com>
2020-04-10 16:09:06 +0200

    Use of insertMany

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -8,17 +8,44 @@ const databaseName = "task-manager";
 
 MongoClient.connect(
   connectionURL,
-  { useNewUrlParser: true },
+  { useNewUrlParser: true, useUnifiedTopology: true },
   (error, client) => {
     if (error) {
       return console.log("Unable to connect to database!");
     }
+    console.log("connected correctly");
 
     const db = client.db(databaseName);
 
-    db.collection("users").insertOne({
-      name: "Piotr",
-      age: 32,
-    });
+    // db.collection("users").insertOne(
+    //   {
+    //     name: "Piotr",
+    //     age: 32,
+    //   },
+    //   (error, result) => {
+    //     if (error) {
+    //       return console.log("unable to insert user");
+    //     }
+    //     console.log(result.ops);
+    //   }
+    // );
+    db.collection("users").insertMany(
+      [
+        {
+          name: "Kate",
+          age: 28,
+        },
+        {
+          name: "Kasprzyk",
+          age: 32,
+        },
+      ],
+      (error, result) => {
+        if (error) {
+          return console.log(error);
+        }
+        console.log(result.ops);
+      }
+    );
   }
 );
