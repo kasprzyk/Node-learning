a3ef03ad0cd7d07bb9d36c636ee736279504abbb
kasprzyk <piotkas1@gmail.com>
2020-04-20 17:35:15 +0200

    Using validator

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+const validator = require("validator");
 // const connectionURL = "mongodb://127.0.0.1:27017";
 
 mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
@@ -18,6 +18,15 @@ const User = mongoose.model("User", {
     validate(value) {
       if (value < 0) {
         throw new Error("Age must be a positive number");
+      }
+    },
+  },
+  email: {
+    type: String,
+    required: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Email is not valid");
       }
     },
   },
