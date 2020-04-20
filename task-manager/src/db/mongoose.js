042b0e7dad59df4edbf1cbbeb1f7defcaa541b4b
kasprzyk <piotkas1@gmail.com>
2020-04-20 17:30:29 +0200

    Validate inside model

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -5,14 +5,21 @@ const mongoose = require("mongoose");
 mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
   useNewUrlParser: true,
   useCreateIndex: true,
+  useUnifiedTopology: true,
 });
 
 const User = mongoose.model("User", {
   name: {
     type: String,
+    required: true,
   },
   age: {
     type: Number,
+    validate(value) {
+      if (value < 0) {
+        throw new Error("Age must be a positive number");
+      }
+    },
   },
 });
 
