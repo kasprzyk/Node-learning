d104b2d7c644262c2aa877bd0be0755d0e63a1e8
kasprzyk <piotkas1@gmail.com>
2020-04-22 17:53:12 +0200

    Created model subfolder

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -8,46 +8,6 @@ mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
   useUnifiedTopology: true,
 });
 
-const User = mongoose.model("User", {
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  password: {
-    type: String,
-    required: true,
-    trim: true,
-    validate(value) {
-      if (value.length < 6) {
-        throw new Error("Password must be longer than 6");
-      }
-      if (validator.contains(value)) {
-        throw new Error("Password cannot contain word password");
-      }
-    },
-  },
-  age: {
-    type: Number,
-    validate(value) {
-      if (value < 0) {
-        throw new Error("Age must be a positive number");
-      }
-    },
-  },
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    lowercase: true,
-    validate(value) {
-      if (!validator.isEmail(value)) {
-        throw new Error("Email is not valid");
-      }
-    },
-  },
-});
-
 const me = new User({
   name: "Piotr",
   age: 32,
---
+++ task-manager/src/models/user.js
@@ -0,0 +1,39 @@
+const User = mongoose.model("User", {
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    validate(value) {
+      if (value.length < 6) {
+        throw new Error("Password must be longer than 6");
+      }
+      if (validator.contains(value)) {
+        throw new Error("Password cannot contain word password");
+      }
+    },
+  },
+  age: {
+    type: Number,
+    validate(value) {
+      if (value < 0) {
+        throw new Error("Age must be a positive number");
+      }
+    },
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Email is not valid");
+      }
+    },
+  },
+});
