7d0cec170a616d78601abb17442301c264843984
kasprzyk <piotkas1@gmail.com>
2020-04-25 12:10:15 +0200

    Promise chaining

--- playground/8-promises.js
+++ playground/8-promises.js
@@ -1,22 +1,19 @@
-const doWorkPromise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve([7, 4, 1]);
-    // reject('Things went wrong!')
-  }, 2000);
-});
-
-doWorkPromise
-  .then((result) => {
-    console.log("Success!", result);
-  })
-  .catch((error) => {
-    console.log("Error!", error);
+const add = (a, b) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(a + b);
+    }, 2000);
   });
+};
 
-//
-//                               fulfilled
-//                              /
-// Promise      -- pending -->
-//                              \
-//                               rejected
-//
+add(1, 1)
+  .then((sum) => {
+    console.log(sum);
+    return add(sum, 4);
+  })
+  .then((sum2) => {
+    console.log(sum2);
+  })
+  .catch((e) => {
+    console.log(e);
+  });
