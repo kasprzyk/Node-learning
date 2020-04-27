edff5623c27f4f3d421e1813b1b41d5b77f75cc5
kasprzyk <piotkas1@gmail.com>
2020-04-27 21:57:37 +0200

    Another async/await

--- task-manager/playground/promise-chaining2.js
+++ task-manager/playground/promise-chaining2.js
@@ -12,3 +12,17 @@ Task.findByIdAndDelete("5e9db6f8a70a98b6f4d31337")
   .catch((e) => {
     console.log(e);
   });
+
+const deleteAndCount = async (id) => {
+  const user = await Task.findByIdAndDelete(id);
+  const count = await Task.countDocuments({ completed: false });
+  return count;
+};
+
+deleteAndCount("5e9db6f8a70a98b6f4d31337")
+  .then((count) => {
+    console.log(count);
+  })
+  .catch((e) => {
+    console.log(e);
+  });
