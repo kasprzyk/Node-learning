1efe7dccff95a13c9a6b227002f8df394a9c1b5a
kasprzyk <piotkas1@gmail.com>
2020-04-28 15:55:52 +0200

    Applying async/await in index.js

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -10,14 +10,13 @@ app.use(express.json());
 
 app.post("/users", (req, res) => {
   const user = new User(req.body);
-  user
-    .save()
-    .then(() => {
-      res.status(201).send(user);
-    })
-    .catch((e) => {
-      res.status(400).send(e);
-    });
+  
+  try {
+    await user.save()
+    res.status(201).send(user);
+  } catch(e) {
+    res.status(201).send(user);
+  }
 });
 
 app.get("/users", (req, res) => {
