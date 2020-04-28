ef39b691b80ba1d779f3066fb00c35b9664c8b1f
kasprzyk <piotkas1@gmail.com>
2020-04-28 16:07:54 +0200

    Refactoring task routes

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -44,41 +44,40 @@ app.get("/users/:id", (req, res) => {
   }
 });
 
-app.post("/tasks", (req, res) => {
-  const task = new Task(req.body);
-  task
-    .save()
-    .then(() => {
-      res.status(201).send(task);
-    })
-    .catch((e) => {
-      res.status(400).send(e);
-    });
-});
+app.post('/tasks', async (req, res) => {
+  const task = new Task(req.body)
 
-app.get("/tasks", (req, res) => {
-  Task.find({})
-    .then((tasks) => {
-      res.send(tasks);
-    })
-    .catch((e) => {
-      res.status(500).send(e);
-    });
-});
+  try {
+      await task.save()
+      res.status(201).send(task)
+  } catch (e) {
+      res.status(400).send(e)
+  }
+})
 
-app.get("/tasks/:id", (req, res) => {
-  const _id = req.params.id;
-  Task.findById(_id)
-    .then((task) => {
+app.get('/tasks', async (req, res) => {
+  try {
+      const tasks = await Task.find({})
+      res.send(tasks)
+  } catch (e) {
+      res.status(500).send()
+  }
+})
+
+app.get('/tasks/:id', async (req, res) => {
+  const _id = req.params.id
+
+  try {
+      const task = await Task.findById(_id)
       if (!task) {
-        return res.status(404).send();
+          return res.status(404).send()
       }
-      res.send(task);
-    })
-    .catch((e) => {
-      res.status(500).send(e);
-    });
-});
+
+      res.send(task)
+  } catch (e) {
+      res.status(500).send()
+  }
+})
 
 app.listen(port, () => {
   console.log("Server is up on port " + port);
