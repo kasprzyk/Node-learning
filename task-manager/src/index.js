7b357083106516931c8208f2696795c102577409
kasprzyk <piotkas1@gmail.com>
2020-04-28 17:05:03 +0200

    Resource updating endpoints

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -8,76 +8,104 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.post("/users", (req, res) => {
+app.post("/users", async (req, res) => {
   const user = new User(req.body);
-  
+
   try {
-    await user.save()
+    await user.save();
     res.status(201).send(user);
-  } catch(e) {
-    res.status(201).send(user);
+  } catch (e) {
+    res.status(400).send(e);
   }
 });
 
-app.get("/users", (req, res) => {
+app.get("/users", async (req, res) => {
   try {
-    const Users = await User.find({})
+    const users = await User.find({});
     res.send(users);
-  }
-  catch (e){
-    res.status(500).send(e);
+  } catch (e) {
+    res.status(500).send();
   }
 });
 
-app.get("/users/:id", (req, res) => {
+app.get("/users/:id", async (req, res) => {
   const _id = req.params.id;
 
   try {
-    const User = await User.findById(_id)
+    const user = await User.findById(_id);
+
     if (!user) {
       return res.status(404).send();
     }
+
     res.send(user);
-  }
-  catch (e){
-    res.status(500).send(e);
+  } catch (e) {
+    res.status(500).send();
   }
 });
 
-app.post('/tasks', async (req, res) => {
-  const task = new Task(req.body)
+app.patch("/users/:id", async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["name", "email", "password", "age"];
+  const isValidOperation = updates.every((update) => {
+    allowedUpdates.includes(update);
+  });
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: "Invalid updates!" });
+  }
 
   try {
-      await task.save()
-      res.status(201).send(task)
-  } catch (e) {
-      res.status(400).send(e)
-  }
-})
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
 
-app.get('/tasks', async (req, res) => {
+    if (!user) {
+      return res.status(404).send();
+    }
+
+    res.send(user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+app.post("/tasks", async (req, res) => {
+  const task = new Task(req.body);
+
   try {
-      const tasks = await Task.find({})
-      res.send(tasks)
+    await task.save();
+    res.status(201).send(task);
   } catch (e) {
-      res.status(500).send()
+    res.status(400).send(e);
   }
-})
+});
 
-app.get('/tasks/:id', async (req, res) => {
-  const _id = req.params.id
+app.get("/tasks", async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.send(tasks);
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+
+app.get("/tasks/:id", async (req, res) => {
+  const _id = req.params.id;
 
   try {
-      const task = await Task.findById(_id)
-      if (!task) {
-          return res.status(404).send()
-      }
+    const task = await Task.findById(_id);
 
-      res.send(task)
+    if (!task) {
+      return res.status(404).send();
+    }
+
+    res.send(task);
   } catch (e) {
-      res.status(500).send()
+    res.status(500).send();
   }
-})
+});
 
 app.listen(port, () => {
   console.log("Server is up on port " + port);
