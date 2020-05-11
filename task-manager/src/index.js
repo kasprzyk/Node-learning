e502eda8e87695174d315723b58dd95a4fbffb25
kasprzyk <piotkas1@gmail.com>
2020-05-11 14:51:35 +0200

    Express middleware

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,27 +1,39 @@
 const express = require("express");
 require("./db/mongoose");
 const userRouter = require("./routers/user");
+const taskRouter = require("./routers/task");
 const User = require("./models/user");
 const Task = require("./models/task");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+// app.use((req, res, next) => {
+//   if (req.method === "GET") {
+//     res.send("GET requests are disabled");
+//   } else {
+//     next();
+//   }
+// });
+app.use((req, res, next) => {
+  res.status(503).send("Site is currently down.");
+});
 app.use(express.json());
 app.use(userRouter);
+app.use(taskRouter);
 
 app.listen(port, () => {
   console.log("Server is up on port " + port);
 });
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcryptjs");
-const myFunction = async () => {
-  const token = jwt.sign({ _id: "abc123" }, "testest", {
-    expiresIn: "7 days",
-  });
-  console.log(token);
-  const data = jwt.verify(token, "testest");
-  console.log(data);
-};
+// const jwt = require("jsonwebtoken");
+// const bcrypt = require("bcryptjs");
+// const myFunction = async () => {
+//   const token = jwt.sign({ _id: "abc123" }, "testest", {
+//     expiresIn: "7 days",
+//   });
+//   console.log(token);
+//   const data = jwt.verify(token, "testest");
+//   console.log(data);
+// };
 
-myFunction();
+// myFunction();
