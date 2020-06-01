e6dc8d5940e76b71fa1128bb8f2b0db6dcfb5d26
kasprzyk <piotkas1@gmail.com>
2020-06-01 22:15:51 +0200

    Multer added

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,37 +1,23 @@
-const express = require("express");
-require("./db/mongoose");
-const userRouter = require("./routers/user");
-const taskRouter = require("./routers/task");
+const express = require('express');
+require('./db/mongoose');
+const userRouter = require('./routers/user');
+const taskRouter = require('./routers/task');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// app.use((req, res, next) => {
-//     if (req.method === 'GET') {
-//         res.send('GET requests are disabled')
-//     } else {
-//         next()
-//     }
-// })
-
-// app.use((req, res, next) => {
-//     res.status(503).send('Site is currently down. Check back soon!')
-// })
+const multer = require('multer');
+const upload = multer({
+  dest: 'images',
+});
+app.post('/upload', upload.single(), (req, res) => {
+  res.send();
+});
 
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
 
 app.listen(port, () => {
-  console.log("Server is up on port " + port);
+  console.log('Server is up on port ' + port);
 });
-
-// const Task = require("./models/task");
-// const User = require("./models/user");
-// const main = async () => {
-//   const task = await Task.findById();
-//   await task.populate("owner").execPopulate();
-//   const user = await Task.findById();
-//   await user.populate("tasks").execPopulate();
-// };
-// main();
