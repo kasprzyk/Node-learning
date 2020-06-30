2fa5d2d4c0936863c7421bea00de120defcdb3af
kasprzyk <piotkas1@gmail.com>
2020-06-30 21:47:03 +0200

    fix

--- task-manager/src/emails/account.js
+++ task-manager/src/emails/account.js
@@ -1,6 +1,5 @@
 const sgMail = require('@sendgrid/mail');
-const sendgridAPIKey =
-  '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';
+const sendgridAPIKey = '';
 
 sgMail.setApiKey(sendgridAPIKey);
 
