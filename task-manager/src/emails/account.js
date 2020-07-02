81983b3d072abce7cd7e6f65e4a5ad70e169d780
kasprzyk <piotkas1@gmail.com>
2020-07-02 22:21:50 +0200

    Welcome email

--- task-manager/src/emails/account.js
+++ task-manager/src/emails/account.js
@@ -3,9 +3,22 @@ const sendgridAPIKey = '';
 
 sgMail.setApiKey(sendgridAPIKey);
 
-sgMail.send({
-  to: 'pkasprzycki@ibpm.pro',
-  from: 'pkasprzycki@ibpm.pro',
-  subject: 'test',
-  text: 'test',
-});
+// sgMail.send({
+//   to: 'pkasprzycki@ibpm.pro',
+//   from: 'pkasprzycki@ibpm.pro',
+//   subject: 'test',
+//   text: 'test',
+// });
+
+const sendWelcomeEmail = (email, name) => {
+  sgMail.send({
+    to: email,
+    from: 'pkasprzycki@ibpm.pro',
+    subject: 'Thanks for joining in!',
+    text: `Welcome to the app, ${name}`,
+  });
+};
+
+module.exports = {
+  sendWelcomeEmail,
+};
