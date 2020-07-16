bd781e1635a83168979d46f2ba0289c43eb3f0e9
kasprzyk <piotkas1@gmail.com>
2020-07-16 11:17:11 +0200

    Writing tests

--- task-manager/tests/math.test.js
+++ task-manager/tests/math.test.js
@@ -1,4 +1,8 @@
-test('test', () => {});
-test('this should fail', () => {
-  throw new Error('failure');
+const { calculateTip } = require('../src/math');
+
+test('Should calculate total with tip', () => {
+  const total = calculateTip(10, 0.3);
+  if (total !== 13) {
+    throw new Error('Total tip should be 13. Got ' + total);
+  }
 });
