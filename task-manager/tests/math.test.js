286ebc2648c8f41b90a1577673089230a207cdbe
kasprzyk <piotkas1@gmail.com>
2020-07-16 11:49:36 +0200

    expect

--- task-manager/tests/math.test.js
+++ task-manager/tests/math.test.js
@@ -2,7 +2,8 @@ const { calculateTip } = require('../src/math');
 
 test('Should calculate total with tip', () => {
   const total = calculateTip(10, 0.3);
-  if (total !== 13) {
-    throw new Error('Total tip should be 13. Got ' + total);
-  }
+  expect(total).toBe(13);
+  //   if (total !== 13) {
+  //     throw new Error('Total tip should be 13. Got ' + total);
+  //   }
 });
