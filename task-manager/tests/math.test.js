95ded5d072bb82569d12684ffbe3119197f83961
kasprzyk <piotkas1@gmail.com>
2020-07-16 14:49:18 +0200

    another tests

--- task-manager/tests/math.test.js
+++ task-manager/tests/math.test.js
@@ -1,4 +1,8 @@
-const { calculateTip } = require('../src/math');
+const {
+  calculateTip,
+  fahrenheitToCelcius,
+  celciusToFahrenheit,
+} = require('../src/math');
 
 test('Should calculate total with tip', () => {
   const total = calculateTip(10, 0.3);
@@ -6,4 +10,19 @@ test('Should calculate total with tip', () => {
   //   if (total !== 13) {
   //     throw new Error('Total tip should be 13. Got ' + total);
   //   }
+});
+
+test('should calculate total with default tip', () => {
+  const total = calculateTip(10);
+  expect(total).toBe(12.5);
+});
+
+test('should calculate celcius to fahrenheit', () => {
+  const temp = celciusToFahrenheit(0);
+  expect(temp).toBe(32);
+});
+
+test('should calculate fahrenheit to celcius', () => {
+  const temp = fahrenheitToCelcius(32);
+  expect(temp).toBe(0);
 });
