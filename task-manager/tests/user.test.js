25d343bbbe0f11fc229430e285b993b04f4f38b2
kasprzyk <piotkas1@gmail.com>
2020-07-21 13:59:01 +0200

    beforeEach, afterEach

--- task-manager/tests/user.test.js
+++ task-manager/tests/user.test.js
@@ -1,5 +1,15 @@
 const request = require('supertest');
 const app = require('../src/app');
+const User = require('../src/models/user');
+
+beforeEach(async () => {
+  console.log('beforeEach');
+  await User.deleteMany();
+});
+
+afterEach(() => {
+  console.log('afterEach');
+});
 
 test('should sign up a new user', async () => {
   await request(app)
